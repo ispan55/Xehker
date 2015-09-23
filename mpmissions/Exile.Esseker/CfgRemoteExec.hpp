9d2368c4942c9161db9390ba6e24f8f1a91ddfb2
WolfkillArcadia <WolfkillArcadia@gmail.com>
2015-09-23 19:12:08 -0400

    updates

--- mpmissions/Exile.Esseker/CfgRemoteExec.hpp
+++ mpmissions/Exile.Esseker/CfgRemoteExec.hpp
@@ -6,9 +6,6 @@ class CfgRemoteExec
 		jip = 0;
 		class fnc_AdminReq { allowedTargets=2; };
 		class ExileServer_system_network_dispatchIncomingMessage { allowedTargets=2; };
-		
-		class FN_infiSTAR_C { allowedTargets=1; };
-		class ExileClient_system_network_dispatchIncomingMessage { allowedTargets=1; };
 	};
 	class Commands
 	{
